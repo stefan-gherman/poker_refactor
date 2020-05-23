723acf48f4c31d87d0c6da75c01335ca5daef7bd
Stefan Gherman <scgheman@gmail.com>
2020-05-24 00:50:39 +0300

    Add javadocs to IllegalInputException class

--- src/com/codecool/customexception/IllegalInputException.java
+++ src/com/codecool/customexception/IllegalInputException.java
@@ -1,5 +1,9 @@
 package com.codecool.customexception;
 
+/**
+ * <p>Custom exception to be raised when input does not follow required patterns</p>
+ * @author Fane Martiru StringBuilder, Domn si Voievod al Tarilor Romane, Archon in Antioch
+ */
 public class IllegalInputException extends Exception {
     public IllegalInputException(String message) {
         super(message);
