fad2708deb13f937593e7f79a9fdeb48eb01f321
Stefan Gherman <scgheman@gmail.com>
2020-05-23 23:33:10 +0300

    Add test for Card Class.

--- test/java/CardTest.java
+++ test/java/CardTest.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
+import com.codecool.customexception.IllegalInputException;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CardTest {
+
+    @Test
+    public void testCardThrowsIllegalArgumentExceptionOnUnsuitableSymbol() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Card("I4");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalArgumentExceptionOnUnsuitableRankBiggerThanMax() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Card("S45");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalArgumentExceptionOnUnsuitableRankSmallerThanMin() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Card("S1");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalArgumentExceptionOnRankNotNumber() {
+        assertThrows(NumberFormatException.class, () -> {
+            new Card("H-");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalInputExceptionOnCardCodeLargerThanTwoChars() {
+        assertThrows(IllegalInputException.class, () -> {
+            new Card("H34353");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalArgumentExceptionOnEmptyRank() {
+        assertThrows(IllegalInputException.class, () -> {
+            new Card("H");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalArgumentExceptionOnEmptySymbol() {
+        assertThrows(IllegalInputException.class, () -> {
+            new Card("4");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalInputExceptionOnEmptyStringSpace() {
+        assertThrows(IllegalInputException.class, () -> {
+            new Card(" ");
+        });
+    }
+
+    @Test
+    public void testCardThrowsIllegalInputExceptionOnEmptyString() {
+        assertThrows(IllegalInputException.class, () -> {
+            new Card("");
+        });
+    }
 }
